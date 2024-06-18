deac624f2596a6850138074a937f9935687cadad
venky-karamkanti <venkykaramkanti7171@gmail.com>
2024-06-19 01:14:53 +0530

    Removed comments

--- src/com/vba/ReverseString.java
+++ src/com/vba/ReverseString.java
@@ -3,13 +3,12 @@ package com.vba;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    String str="Gudda muyyu";
-    str=str.toLowerCase();
-    char[] ch=str.toCharArray();
-    for(int i=str.length()-1;i>0;i--) {
-    	System.out.print(ch[i]);
-    }
+		String str="Gudda muyyu";
+		str=str.toLowerCase();
+		char[] ch=str.toCharArray();
+		for(int i=str.length()-1;i>0;i--) {
+			System.out.print(ch[i]);
+		}
 	}
 
 }
