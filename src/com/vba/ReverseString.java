a95b0e3525eb09cd0cf7b2de7e09cb213e9b657a
venky-karamkanti <venkykaramkanti7171@gmail.com>
2024-06-19 00:53:00 +0530

    Second File

---
+++ src/com/vba/ReverseString.java
@@ -0,0 +1,15 @@
+package com.vba;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String str="Gudda muyyu";
+    str=str.toLowerCase();
+    char[] ch=str.toCharArray();
+    for(int i=str.length()-1;i>0;i--) {
+    	System.out.print(ch[i]);
+    }
+	}
+
+}
